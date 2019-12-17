f35e69c3be5005ae93c3d986767c1a800e27e913
JitaYui <shugochara13456@gmail.com>
2019-12-17 11:53:28 +0800

    using swing to make gui

---
+++ src/Client.java
@@ -0,0 +1,21 @@
+import javax.print.attribute.standard.Finishings;
+import javax.swing.*;
+
+public class Client {
+    /* The client class is used for creating a gui order menu.*/
+    private void Init() {
+        /*initial the gui*/
+        //TODO: layout
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Hello World");
+        jFrame.getContentPane().add(label);
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.Init();
+    }
+}
